0a8131df8a8e67344a9faa73318d8165aa9288cb
Adam Patterson <Adam Patterson>
2014-07-12 10:11:40 -0400

    adds paperclip gem

--- app/models/photo_shout.rb
+++ app/models/photo_shout.rb
@@ -1,5 +1,4 @@
 class PhotoShout < ActiveRecord::Base
-  has_attached_file :image, styles: {
-    shout: "200x200>"
-  }
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 end
